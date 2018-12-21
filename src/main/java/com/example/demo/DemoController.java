4b8bfd655b47ccca3c9957344b9235121d50a70b
hiru weerasinghe <abcd@gmail.com>
2018-12-21 09:03:47 +0530

    helloo method added

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,16 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
 
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String sayHello() {
+		System.out.println("Helloooooooooo :) ");
+		return new String("Helloooo");	
+	
+	}
 }
