375146ef6ae76140115c1aba57912071e32928ba
hiru weerasinghe <abcd@gmail.com>
2019-02-06 13:42:45 +0530

    Student file added

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Integer marks;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getMarks() {
+		return marks;
+	}
+	public void setMarks(Integer marks) {
+		this.marks = marks;
+	}
+}
